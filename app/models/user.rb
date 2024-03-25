c518bcdf96959ae4b8602b7f8d7a42f59f3f702d
Pradhuman Tomar <pstPro01@gmail.com>
2024-03-25 20:34:21 +0530

    added validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true
+  validates_confirmation_of :password
 
   before_save :encrypt_password
 
