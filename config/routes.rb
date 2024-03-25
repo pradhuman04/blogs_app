92ffeff5875a164f9020ff759889ba18dbc9f678
Pradhuman Tomar <pstPro01@gmail.com>
2024-03-25 14:52:38 +0530

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  root 'users#new'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  resources :users, only: [:new, :create]
+  resources :blogs, only: [:index]
+
+  # API routes
+  namespace :api do
+    namespace :v1 do
+      post 'login', to: 'sessions#create'
+      post 'register', to: 'users#create'
+    end
+  end
+end
